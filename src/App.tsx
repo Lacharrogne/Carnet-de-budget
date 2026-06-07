90a30325ce5736be993db60a0273de27cea60a42
Lacharrogne <maxime1.charron@epitech.eu>
2026-06-07 20:28:15 +0200

    Add scroll to top on navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { Navigate, Route, Routes } from 'react-router'
+import { Route, Routes } from 'react-router'
 
 import AppLayout from './components/layout/AppLayout'
+import ScrollToTop from './components/layout/ScrollToTop'
 import AccountsPage from './pages/AccountsPage'
 import BudgetsPage from './pages/BudgetsPage'
 import CalendarPage from './pages/CalendarPage'
@@ -15,24 +16,26 @@ import TransactionsPage from './pages/TransactionsPage'
 
 export default function App() {
   return (
-    <AppLayout>
-      <Routes>
-        <Route path="/" element={<DashboardPage />} />
-        <Route path="/comptes" element={<AccountsPage />} />
-        <Route path="/transactions" element={<TransactionsPage />} />
-        <Route path="/budgets" element={<BudgetsPage />} />
-        <Route path="/abonnements" element={<RecurringPaymentsPage />} />
-        <Route path="/recurrents" element={<RecurringPaymentsPage />} />
-        <Route path="/dettes" element={<DebtsPage />} />
-        <Route path="/patrimoine" element={<NetWorthPage />} />
-        <Route path="/investissements" element={<InvestmentsPage />} />
-        <Route path="/objectifs" element={<GoalsPage />} />
-        <Route path="/calendrier" element={<CalendarPage />} />
-        <Route path="/statistiques" element={<StatsPage />} />
-        <Route path="/stats" element={<StatsPage />} />
+    <>
+      <ScrollToTop />
 
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </AppLayout>
+      <AppLayout>
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/comptes" element={<AccountsPage />} />
+          <Route path="/transactions" element={<TransactionsPage />} />
+          <Route path="/budgets" element={<BudgetsPage />} />
+          <Route path="/abonnements" element={<RecurringPaymentsPage />} />
+          <Route path="/recurrents" element={<RecurringPaymentsPage />} />
+          <Route path="/dettes" element={<DebtsPage />} />
+          <Route path="/patrimoine" element={<NetWorthPage />} />
+          <Route path="/investissements" element={<InvestmentsPage />} />
+          <Route path="/objectifs" element={<GoalsPage />} />
+          <Route path="/calendrier" element={<CalendarPage />} />
+          <Route path="/statistiques" element={<StatsPage />} />
+          <Route path="/stats" element={<StatsPage />} />
+        </Routes>
+      </AppLayout>
+    </>
   )
 }
